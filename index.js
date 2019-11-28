d590c38620d0c3c39f7654d3afa2df146553438b
Arjun von Hatten <arjun.vonhatten@vidyard.com>
2019-11-28 14:18:53 -0500

    try this

--- index.js
+++ index.js
@@ -20,89 +20,59 @@ app.use(bodyParser.json())
 app.use(poweredByHandler)
 
 // --- SNAKE LOGIC GOES BELOW THIS LINE ---
-// numSnakes = 0;
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
 
 // Handle POST request to '/start'
 app.post('/start', (request, response) => {
-  // numSnakes++;
-  // Response data
-  var returnData = {
-      color: "#00CC66",
-      name: "Max Power",
-      head_url: "https://emoji.slack-edge.com/T02GDU01L/chill/7c582ab11138db5e.png",
-      taunt: "Catch these hands",
-  };
+  // NOTE: Do something here to start the game
 
-  return response.json(returnData)
+  // Response data
+  const data = {
+    color: '#DFFF00',
+  }
+
+  nextMove = 'left';
+
+  return response.json(data)
 })
+
+let lastMove = 'left';
+
+function generateNextMove() {
+  if (lastMove === 'up') {
+    return 'right';
+  }
+
+  if (lastMove === 'right') {
+    return 'down';
+  }
+
+  if (lastMove === 'down') {
+    return 'left'
+  }
+
+  if (lastMove === 'left') {
+    return 'up';
+  }
+
+  return 'down';
+}
+
 
 // Handle POST request to '/move'
-app.post('/move', (req, res) => {
-  try {
-    // var reqBodyHistory = updateAndGetHistory(req.body);
-    // console.log('Found game history with :' + reqBodyHistory.length + ' moves.');
-    // console.log(req.body.turn);
-    var responseData = {
-        move: getMyMove(), //(req.body, reqBodyHistory),
-        taunt: foosTaunt(), // optional, but encouraged!
-    };
-    return res.json(responseData);
+app.post('/move', (request, response) => {
+  // NOTE: Do something here to generate your move
 
-} catch (err) {
-    if (err.message) {
-        console.log('\nMessage: ' + err.message);
-    }
-    if (err.stack) {
-        console.log('\nStacktrace:');
-        console.log('====================');
-        console.log(err.stack);
-    }
-}
+  const nextMove = generateNextMove();
+
+  // Response data
+  const data = {
+    move: nextMove, // one of: ['up','down','left','right']
+  }
+
+  lastMove = nextMove;
+
+  return response.json(data)
 })
-
-function foosTaunt() {
-  return 'Rack City';
-}
-
-function getMyMove() {
-  move_arr = ['up','down','left','right']
-  index = getRandomInt(4);
-  retval = move_arr[index];
-  console.log('HERE!!!');
-  console.log(retval);
-  return retval;
-}
-
-// function updateAndGetHistory(reqBody) {
-//   // game id doesnt change when you press 'q' to reset game. so if turn is 0, reset this game
-//   var turn = reqBody.turn;
-//   var gameID = reqBody.game_id;
-
-//   var i = 0;
-//   for (var reqBodyHistory of reqBodyHistories) {
-//       if (reqBodyHistory[0].game_id == gameID) {
-
-//           break;
-//       }
-//       i++;
-//   }
-//   if (i == reqBodyHistories.length) {
-//       // if this game not yet in histories - add it
-//       // begin a new game history
-//       lastDiffTailByGame.push([]);
-//       // set game history index to newest addition
-//       i = reqBodyHistories.length - 1;
-//   }
-//   // if turn is 0, reset this game.
-//   if (turn == 0) {
-//       reqBodyHistories[i] = [];
-//   }
-//   reqBodyHistories[i].push(reqBody);
-//   return reqBodyHistories[i];
-// }
 
 app.post('/end', (request, response) => {
   // NOTE: Any cleanup when a game is complete.
